5b18a047d82b0b9167aa111ab3d4de2e4e943985
mac <mac@mac.com>
2019-10-16 23:37:28 +0200

    rendu

---
+++ spark-streaming/src/main/scala/com/efrei/phimobi/StreamCreation.scala
@@ -0,0 +1,42 @@
+package com.efrei.phimobi
+
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+
+
+
+object StreamCreation extends App {
+
+  case class HelloWorld(message: String)
+
+  val conf = new SparkConf().setAppName("test").setMaster("local")
+  val ssc = new StreamingContext(conf, Seconds(1))
+
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> "localhost:9092",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "use_a_separate_group_id_for_each_stream",
+    "auto.offset.reset" -> "latest",
+    "enable.auto.commit" -> (false: java.lang.Boolean)
+  )
+
+  val topics = Array("success")
+  val stream = KafkaUtils.createDirectStream[String, String](
+    ssc,
+    PreferConsistent,
+    Subscribe[String, String](topics, kafkaParams)
+  )
+
+  stream.map(record => (record.key, record.value)).saveAsTextFiles("./spark-warehouse/res")
+
+  ssc.start()
+  ssc.awaitTermination()
+
+
+
+}
